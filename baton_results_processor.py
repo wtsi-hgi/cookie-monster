bcf9975319ff157f987275f0b06ed14aaca32f2a
Irina Colgiu <ic4@sanger.ac.uk>
2015-09-23 12:35:10 +0100

    Imported code from meta-check and adjust it for current needs

---
+++ baton_results_processor.py
@@ -0,0 +1,64 @@
+"""
+Copyright (C) 2015  Genome Research Ltd.
+
+Author: Irina Colgiu <ic4@sanger.ac.uk>
+
+This program is part of cookie-monster
+
+cookie-monster is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+This file has been created on Sep 22, 2015.
+"""
+
+from collections import defaultdict, namedtuple
+import json
+
+MetaAVU = namedtuple('MetaAVU', ['attribute', 'value'])
+
+def from_metaquery_results_to_fpaths_and_avus(search_results_json):
+    """
+        This method takes as parameter the json result of a metaquery containing avus and checksum,
+        and turns the json into a dict having as key a fpath, and as value: dict of
+        {'avus' : [MetaAVU(), MetaAVU()], 'checksum' : 'the_result_of_ichksum'}
+    :param search_results_json:
+    :param filters: optional (not implemented yet)
+    :return: dict key = fpath, value = {'avus' : [MetaAVU(), MetaAVU()], 'checksum' : 'the_result_of_ichksum'}
+    """
+    files_with_chksum_and_avus = defaultdict(dict)
+    print "DATA objects: " + str(search_results_json)
+    for data_obj in json.loads(search_results_json):
+        coll = None
+        fname = None
+        do_avus = []
+        do_checksum = None
+
+        for do_item, do_item_val in data_obj.items():
+            if do_item == 'collection':
+                coll = do_item_val
+            elif do_item == 'data_object':
+                fname = do_item_val
+            elif do_item == 'avus':
+                for avu in do_item_val:
+                    avu_obj = MetaAVU(attribute=str(avu['attribute']), value=str(avu['value']))  # MetaAVU = namedtuple('MetaAVU', ['attribute', 'value'])    # list of attribute-value tuples
+                    do_avus.append(avu_obj)
+            elif do_item == 'checksum':
+                do_checksum = str(do_item_val)
+
+            # sometimes there is an error here!!!!!
+
+            # TODO: can add also: 1) data access checks, 2) replicate checksum checks, plus verify there are actually 2 replicas of each DO
+        fpath = os.path.join(coll, fname)
+        files_with_chksum_and_avus[fpath] = {'avus' : do_avus, 'checksum' : do_checksum}
+    return files_with_chksum_and_avus
+
+
+
---
+++ notifier.py
@@ -0,0 +1,35 @@
+"""
+Copyright (C) 2015  Genome Research Ltd.
+
+Author: Irina Colgiu <ic4@sanger.ac.uk>
+
+This program is part of cookie-monster
+
+cookie-monster is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+This file has been created on Sep 22, 2015.
+"""
+
+import baton_wrapper as baton
+import baton_results_processor
+import pdb
+
+def fetch_irods_metadata(fpath):
+    #pdb.set_trace()
+    metaquery_results = baton.BatonAPI.query_by_filepath_for_file_metadata(fpath)
+    fpath_avus = baton_results_processor.from_metaquery_results_to_fpaths_and_avus(metaquery_results)  # this is a dict of key = fpath, value = dict({'avus':[], 'checksum':str})
+    print str(fpath_avus)
+
+def main():
+    fetch_irods_metadata('/seq/17426/17426_8#7.cram')
+
+main()
