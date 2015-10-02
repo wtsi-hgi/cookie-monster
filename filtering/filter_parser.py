50b47cb96ab3be7ca5c2f09dedb1403e2cd0da63
Irina Colgiu <ic4@sanger.ac.uk>
2015-10-02 11:33:32 +0100

    Extended the filter parser functionality

--- filtering/filter_parser.py
+++ filtering/filter_parser.py
@@ -20,10 +20,11 @@ This file has been created on Sep 28, 2015.
 """
 
 import ConfigParser
-from filter_type import FilterOptions
+from com import exceptions
+from filter_type import FilesFilter
 
 def parse_section(config, section):
-    section_fields = FilterOptions()
+    section_fields = {}
     if config.has_section(section):
         section_opts = config.items(section)
         for opt in section_opts:
@@ -31,29 +32,40 @@ def parse_section(config, section):
             opt_value = opt[1]
             if opt[1].find('\n') != -1:
                 opt_value = opt_value.split('\n')
-            setattr(section_fields, opt_name, opt_value)
+            section_fields[opt_name] = opt_value
     return section_fields
 
+def check_filters_correct(incl_filters, excl_filters):
+    if set(incl_filters).intersection(set(excl_filters)):
+        raise ValueError("The filters aren't correct, you can't include one filter and exclude it at the same time.")
+
+def change_filters_format(incl_filters, excl_filters):
+    filters = {}
+    print str(incl_filters)
+    print str(excl_filters)
+    for filter_name, val in incl_filters.items():
+        filters[filter_name] = FilesFilter(filter_name, val, 'INCLUDE')
+    for filter_name, val in excl_filters.items():
+        filters[filter_name] = FilesFilter(filter_name, val, 'EXCLUDE')
+    return filters
 
 def parse_filter_file(fpath):
     config = ConfigParser.ConfigParser()
     config.read(fpath)
     included_filter = parse_section(config, 'INCLUDE')
     excluded_filter = parse_section(config, 'EXCLUDE')
-    return included_filter, excluded_filter
+    try:
+        check_filters_correct(included_filter, excluded_filter)
+    except ValueError as e:
+        raise exceptions.WrongConfigFile(fpath, e.message)
+    else:
+        filters = change_filters_format(included_filter, excluded_filter)
+        return filters
 
 
-incl, excl = parse_filter_file('/nfs/users/nfs_i/ic4/Projects/cookie-monster/cookie-monster/filters.txt')
-print "included" + str(vars(incl))
-print "excluded" + str(vars(excl))
-
-
-# config.read('/nfs/users/nfs_i/ic4/Projects/cookie-monster/cookie-monster/filters.txt')
-
-# config.sections()
-#config.has_section('INCLUDAAE')#
-# if config.has_option('INCLUDE', 'TARGET'):
-#     target = config.get('INCLUDE', 'TARGET')
+# incl, excl = parse_filter_file('/nfs/users/nfs_i/ic4/Projects/cookie-monster/cookie-monster/filters.txt')
+# print "included" + str(vars(incl))
+# print "excluded" + str(vars(excl))
 
 
 
