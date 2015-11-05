31e5a3123145e171ab3a552d5eb5ddeea8013980
Christopher Harrison <ch12@sanger.ac.uk>
2015-11-05 15:03:36 +0000

    Skeleton data manager

---
+++ cookiemonster/datamanager/manager.py
@@ -0,0 +1,12 @@
+# GPLv3 or later
+# Copyright (c) 2015 Genome Research Limited
+
+'''
+# Data Manager
+foo...
+'''
+
+from cookiemonster.common.models import Filepdate
+
+class DataManager(object):
+    pass
