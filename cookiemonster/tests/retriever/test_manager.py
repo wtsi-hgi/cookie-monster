3719830428b047a758a2d08f3ef77e0a8c51d248
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-11-09 13:27:35 +0000

    Commenting and testing of retrival manager.

--- cookiemonster/tests/retriever/test_manager.py
+++ cookiemonster/tests/retriever/test_manager.py
@@ -37,14 +37,14 @@ class TestRetrievalManager(unittest.TestCase):
 
         # Force retrieval manager to only do one cycle
         def do_one_cycle(*args):
-            self._retrieval_manager._set_timer_for_next_periodic_retrieve.side_effect = MagicMock()
+            self._retrieval_manager._schedule_next_periodic_retrieve.side_effect = MagicMock()
             self._retrieval_manager._do_retrieve_periodically(*args)
-        self._retrieval_manager._set_timer_for_next_periodic_retrieve = MagicMock(side_effect=do_one_cycle)
+        self._retrieval_manager._schedule_next_periodic_retrieve = MagicMock(side_effect=do_one_cycle)
 
         # Override current time to make deterministic
         RetrievalManager._get_current_time = MagicMock(return_value=TestRetrievalManager._CURRENT_TIME)
 
-    def test__do_retrieve(self):
+    def test__do_retrieve_with_file_updates(self):
         # Setup
         listener = MagicMock()
         self._retrieval_manager.add_listener(listener)
@@ -65,11 +65,34 @@ class TestRetrievalManager(unittest.TestCase):
                 TestRetrievalManager._TIME_TAKEN_TO_DO_RETRIEVE)
         )
 
+    def test__do_retrieve_without_file_updates(self):
+        # Setup
+        listener = MagicMock()
+        self._retrieval_manager.add_listener(listener)
+        self._retrieval_log_mapper.add = MagicMock()
+        self._file_updates.clear()
+        self._retrieval_manager._latest_retrieved_timestamp = TestRetrievalManager._CURRENT_TIME - timedelta(days=1)
+
+        # Call SUT method
+        self._retrieval_manager._do_retrieve(TestRetrievalManager._SINCE)
+
+        # Assert that retrieves updates from source
+        self._file_update_retriever.query_for_all_file_updates_since.assert_called_once_with(
+            TestRetrievalManager._SINCE)
+        # Assert that updates listener has not been called given that there are no file updates
+        listener.assert_not_called()
+        # Assert that retrieval is logged but that latest retrieved timestamp has not changed
+        self._retrieval_log_mapper.add.assert_called_once_with(
+            RetrievalLog(
+                self._retrieval_manager._latest_retrieved_timestamp, len(self._file_updates),
+                TestRetrievalManager._TIME_TAKEN_TO_DO_RETRIEVE)
+        )
+
     def test__do_retrieve_periodically(self):
         # Setup
         self._retrieval_manager._do_retrieve = MagicMock()
         retrieval_scheduled_for = TestRetrievalManager._SINCE
-        self._retrieval_manager._set_timer_for_next_periodic_retrieve = MagicMock()
+        self._retrieval_manager._schedule_next_periodic_retrieve = MagicMock()
 
         # Call SUT method
         self._retrieval_manager._do_retrieve_periodically(retrieval_scheduled_for)
@@ -77,7 +100,7 @@ class TestRetrievalManager(unittest.TestCase):
         # Assert that _do_retrieve was called
         self._retrieval_manager._do_retrieve.assert_called_once_with(retrieval_scheduled_for)
         # Assert that next cycle was scheduled
-        self._retrieval_manager._set_timer_for_next_periodic_retrieve.assert_called_once_with(
+        self._retrieval_manager._schedule_next_periodic_retrieve.assert_called_once_with(
             retrieval_scheduled_for + TestRetrievalManager._RETRIEVAL_PERIOD)
 
     def test_start(self):
@@ -85,12 +108,10 @@ class TestRetrievalManager(unittest.TestCase):
         self._retrieval_manager.start()
 
         # Assert started periodic retrieval for file updates since the correct time
-        self._retrieval_manager._set_timer_for_next_periodic_retrieve.assert_has_calls([
+        self._retrieval_manager._schedule_next_periodic_retrieve.assert_has_calls([
             call(TestRetrievalManager._CURRENT_TIME),
             call(TestRetrievalManager._CURRENT_TIME + TestRetrievalManager._RETRIEVAL_PERIOD)
         ], any_order=False)
-
-    # TODO: Test no retrieved with _do_retrieval
 
 
 if __name__ == '__main__':
