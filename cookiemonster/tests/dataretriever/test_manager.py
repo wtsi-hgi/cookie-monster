b1e2732f37be79df32ddcb7639619daf5a3e01d7
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-11-09 10:08:47 +0000

    Added tests for data retrieval manager.

--- cookiemonster/tests/dataretriever/test_manager.py
+++ cookiemonster/tests/dataretriever/test_manager.py
@@ -10,21 +10,25 @@ from cookiemonster.tests.dataretriever.stubs import StubRetrievalLogMapper
 
 
 class TestRetrievalManager(unittest.TestCase):
-    _TIME_DELTA = timedelta.resolution
-    _DATE_TIME = datetime.min
+    _TIME_DELTA = timedelta(seconds=10)
+    _DATE_TIME = date.min
+    _CURRENT_TIME = datetime(year=2000, month=2, day=1)
 
     def setUp(self):
-        self._retrieval_period = timedelta(seconds=2)
+        self._retrieval_period = TestRetrievalManager._TIME_DELTA
         self._file_update_retriever = StubFileUpdateRetriever()
         self._retrieval_log_mapper = StubRetrievalLogMapper()
         self._retrieval_manager = RetrievalManager(
             self._retrieval_period, self._file_update_retriever, self._retrieval_log_mapper)
-        # Disable timer by default
-        self._retrieval_manager._set_timer_for_next_periodic_retrieve = MagicMock()
 
         self._file_updates = FileUpdateCollection()
-        query_result = QueryResult(self._file_updates, TestRetrievalManager._TIME_DELTA)
-        self._file_update_retriever.query_for_all_file_updates_since = MagicMock(return_value=query_result)
+        self._query_result = QueryResult(self._file_updates, TestRetrievalManager._TIME_DELTA)
+        self._file_update_retriever.query_for_all_file_updates_since = MagicMock(return_value=self._query_result)
+
+        # Disable timer
+        self._retrieval_manager._set_timer_for_next_periodic_retrieve = MagicMock()
+        # Override current time
+        RetrievalManager._get_current_time = MagicMock(return_value=TestRetrievalManager._CURRENT_TIME)
 
     def test__do_retrieve(self):
         listener = MagicMock()
@@ -38,54 +42,30 @@ class TestRetrievalManager(unittest.TestCase):
             TestRetrievalManager._DATE_TIME)
         # Check that updates listener
         listener.assert_called_once_with(self._file_updates)
-        # Check that retrieval is logged
-        # self._retrieval_log_mapper.add.assert_called_once_with(RetrievalLog())
-        # latest_retrieved_timestamp: datetime, number_of_file_updates: int, time_taken_to_complete_query: timedelta
+        # FIXME: Check that retrieval is logged
+        # self._retrieval_log_mapper.add.assert_called_once_with(
+        #     RetrievalLog(TestRetrievalManager._CURRENT_TIME, len(self._file_updates), TestRetrievalManager._TIME_DELTA))
 
+    def test__do_retrieve_periodically(self):
+        self._retrieval_manager._do_retrieve = MagicMock()
+        retrieval_scheduled_for = TestRetrievalManager._DATE_TIME
 
+        self._retrieval_manager._do_retrieve_periodically(retrieval_scheduled_for)
 
-    #
-    # def test_start_to_do_retrieve_with_no_updates(self):
-    #     file_updates = FileUpdateCollection()
-    #     query_result = QueryResult(file_updates,  timedelta.resolution)
-    #     self._file_update_retriever.query_for_all_file_updates_since = MagicMock(return_value=query_result)
-    #     listener = MagicMock()
-    #     self._retrieval_manager.add_listener(listener)
-    #     self._retrieval_manager.start()
-    #
-    #     self._file_update_retriever.query_for_all_file_updates_since.assert_called_once_with(date.min)
-    #     listener.assert_called_once_with(file_updates)
-    #
-    # def test_start_to_do_retrieve_with_updates(self):
-    #     file_updates = FileUpdateCollection(
-    #         [FileUpdate("", "", date.min + timedelta.resolution), FileUpdate("", "", date.max)])
-    #     query_result = QueryResult(file_updates,  timedelta.resolution)
-    #     self._file_update_retriever.query_for_all_file_updates_since = MagicMock(return_value=query_result)
-    #     listener = MagicMock()
-    #     self._retrieval_manager.add_listener(listener)
-    #     self._retrieval_manager.start()
-    #
-    #     self._file_update_retriever.query_for_all_file_updates_since.assert_called_once_with(date.min)
-    #     listener.assert_called_once_with(file_updates)
-    #     self.assertEquals(self._retrieval_manager._latest_retrieved_timestamp, date.max)
-    #
-    # def test_start_with_file_updates_since(self):
-    #     file_updates_since = date.min + timedelta.resolution
-    #     file_updates = FileUpdateCollection(
-    #         [FileUpdate("", "", date.min), FileUpdate("", "", file_updates_since), FileUpdate("", "", date.max)])
-    #     query_result = QueryResult(file_updates[1:],  timedelta.resolution)
-    #     self._file_update_retriever.query_for_all_file_updates_since = MagicMock(return_value=query_result)
-    #     listener = MagicMock()
-    #     self._retrieval_manager.add_listener(listener)
-    #     self._retrieval_manager.start(file_updates_since)
-    #
-    #     self._file_update_retriever.query_for_all_file_updates_since.assert_called_once_with(file_updates_since)
-    #     listener.assert_called_once_with(file_updates[1:])
-    #     self.assertEquals(self._retrieval_manager._latest_retrieved_timestamp, date.max)
-    #
-    # def test_start_correctly_schedules_next(self):
-    #     # TODO
-    #     raise NotImplementedError()
+        self._retrieval_manager._do_retrieve.assert_called_once_with(retrieval_scheduled_for)
+        self._retrieval_manager._set_timer_for_next_periodic_retrieve.assert_called_once_with(
+            retrieval_scheduled_for + self._retrieval_period)
+
+    def test__log_retrieval(self):
+        self._retrieval_manager._log_retrieval(self._query_result)
+        self._retrieval_log_mapper.add = MagicMock()
+        # self._retrieval_log_mapper.
+        # FIXME: Complete when log mapper interface is done
+
+    def test_start(self):
+        self._retrieval_manager.start()
+        self._retrieval_manager._set_timer_for_next_periodic_retrieve.assert_called_once_with(
+            TestRetrievalManager._CURRENT_TIME)
 
 
 if __name__ == '__main__':
