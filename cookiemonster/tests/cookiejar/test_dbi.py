241bab9b7961347887fa7317a1e952a4fea32f96
Christopher Harrison <ch12@sanger.ac.uk>
2015-11-27 14:13:27 +0000

    Begin updating CouchDB interface

--- cookiemonster/tests/cookiejar/test_dbi.py
+++ cookiemonster/tests/cookiejar/test_dbi.py
@@ -1,3 +1,5 @@
+# GPLv3 or later
+# Copyright (c) 2015 Genome Research Limited
 import unittest
 from unittest import mock
 
@@ -5,14 +7,14 @@ from cookiemonster.cookiejar._dbi import DBI
 
 
 class TestDBI(unittest.TestCase):
-    _DB   = 'foo'
     _HOST = 'bar'
+    _DB   = 'foo'
 
     @mock.patch('cookiemonster.cookiejar._dbi.couchdb')
     def test_metadata_init(self, mock_couch):
         # Connect and create
         mock_couch.Server().__contains__.return_value = False
-        _ = DBI(TestDBI._DB, TestDBI._HOST)
+        _ = DBI(TestDBI._HOST, TestDBI._DB)
 
         mock_couch.Server.assert_called_with(TestDBI._HOST)
         mock_couch.Server().__contains__.assert_called_with(TestDBI._DB)
@@ -22,9 +24,11 @@ class TestDBI(unittest.TestCase):
         # Connect to existing
         mock_couch.reset_mock()
         mock_couch.Server().__contains__.return_value = True
-        _ = DBI(TestDBI._DB, TestDBI._HOST)
+        _ = DBI(TestDBI._HOST, TestDBI._DB)
 
         mock_couch.Server().create.assert_not_called()
+
+    # TODO Test other methods...
 
 if __name__ == '__main__':
     unittest.main()
