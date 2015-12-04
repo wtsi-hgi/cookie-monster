f6dd8bb3e3f04742880c6781458fb21edf35bee9
Christopher Harrison <ch12@sanger.ac.uk>
2015-12-04 15:02:47 +0000

    BiscuitTin: CouchDB-backed CookieJar

--- cookiemonster/cookiejar/main.py
+++ cookiemonster/cookiejar/main.py
@@ -8,11 +8,10 @@ Exportable Classes: `BiscuitTin`, `CookieJar` (by proxy)
 BiscuitTin
 ----------
 `BiscuitTin` implements the interface decreed by `CookieJar`. It must be
-instantiated with the CouchDB host URL and database name prefix. It will
-connect to (and create, wherever necessary) two databases on the
-provided host: `{prefix}-queue` and `{prefix}-metadata`, which will
-manage the processing queue and metadata for files (using their path as
-document IDs), respectively.
+instantiated with the CouchDB host URL and database name. It will
+connect to (and create, if necessary) said database on the host and set
+up (again, if necessary) the required design documents to manage the
+processing queue and metadata repository.
 
 `BiscuitTin` implements `Listenable`; when metadata is enriched, it will
 broadcast the current queue length to all downstream listeners.
@@ -35,33 +34,44 @@ from cookiemonster.cookiejar._cookiejar import CookieJar
 from cookiemonster.cookiejar._dbi import Bert, Ernie
 
 class BiscuitTin(CookieJar):
-    def __init__(self, db_host: str, db_prefix: str):
+    def __init__(self, db_host: str, db_name: str):
         '''
         Constructor: Initialise the database connections
 
-        @param  db_host    Database host URL
-        @param  db_prefix  Database name prefix
+        @param  db_host  Database host URL
+        @param  db_name  Database name
         '''
-        self._queue = Bert(db_host, '{}-queue'.format(db_prefix))
-        self._metadata = Ernie(db_host, '{}-metadata'.format(db_prefix))
+        self._queue = Bert(db_host, db_name)
+        self._metadata = Ernie(db_host, db_name)
 
         # Initialise listeners 
         super().__init__()
 
     def enrich_cookie(self, path: str, enrichment: Enrichment):
-        pass
+        self._queue.mark_dirty(path)
+        self._metadata.add_metadata(path, enrichment)
+        self.notify_listeners(self.queue_length())
 
     def mark_as_failed(self, path: str, requeue_delay: timedelta):
-        pass
+        self._queue.mark_finished(path)
+        self._queue.mark_dirty(path, requeue_delay)
 
     def mark_as_complete(self, path: str):
-        pass
+        self._queue.mark_finished(path)
 
     def mark_as_reprocess(self, path: str):
-        pass
+        self._queue.mark_dirty(path)
 
     def get_next_for_processing(self) -> Optional[Cookie]:
-        pass
+        to_process = self._queue.dequeue()
+
+        if to_process is None:
+            return None
+
+        cookie = Cookie(to_process)
+        cookie.enrichments = self._metadata.get_metadata(to_process)
+
+        return cookie
 
     def queue_length(self) -> int:
-        pass
+        return self._queue.queue_length()
