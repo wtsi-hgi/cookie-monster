b0058edfc94229019b6aaf3a39a9e268a749df64
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-12-16 16:54:59 +0000

    Created Docker image that is able to run the tests using Docker-in-Docker (dind).

--- cookiemonster/tests/common/test_collections.py
+++ cookiemonster/tests/common/test_collections.py
@@ -7,7 +7,7 @@ from cookiemonster.common.models import Update
 from cookiemonster.common.collections import UpdateCollection
 
 
-class TestFileUpdateCollection(unittest.TestCase):
+class TestUpdateCollection(unittest.TestCase):
     """
     Tests for `UpdateCollection`.
     """
@@ -18,38 +18,38 @@ class TestFileUpdateCollection(unittest.TestCase):
         self.assertEquals(len(UpdateCollection()), 0)
 
     def test_can_instantiate_with_list(self):
-        file_updates_list = [
+        updates_list = [
             Update("", "", datetime.min, self.metadata),
             Update("", "", datetime.min, self.metadata)
         ]
-        file_updates = UpdateCollection(file_updates_list)
-        self.assertCountEqual(file_updates, file_updates_list)
+        updates = UpdateCollection(updates_list)
+        self.assertCountEqual(updates, updates_list)
 
-    def test_can_instantiate_with_file_update_collection(self):
-        file_updates1= UpdateCollection([
+    def test_can_instantiate_with_update_collection(self):
+        updates_1= UpdateCollection([
             Update("", "", datetime.min, self.metadata),
             Update("", "", datetime.min, self.metadata)
         ])
-        file_updates2 = UpdateCollection(file_updates1)
-        self.assertCountEqual(file_updates2, file_updates1)
+        updates_2 = UpdateCollection(updates_1)
+        self.assertCountEqual(updates_2, updates_1)
 
     def test_get_most_recent_when_empty(self):
         self.assertRaises(ValueError, UpdateCollection().get_most_recent)
 
     def test_get_most_recent(self):
-        file_updates = UpdateCollection([
+        updates = UpdateCollection([
             Update("", "", datetime.max, self.metadata),
             Update("", "", datetime.min, self.metadata)
         ])
-        self.assertCountEqual(file_updates.get_most_recent(), [file_updates[0]])
+        self.assertCountEqual(updates.get_most_recent(), [updates[0]])
 
     def test_get_most_recent_when_many_have_same_latest(self):
-        file_updates = UpdateCollection([
+        updates = UpdateCollection([
             Update("", "", datetime.max, self.metadata),
             Update("", "", datetime.min, self.metadata),
             Update("", "", datetime.max, self.metadata)
         ])
-        self.assertCountEqual(file_updates.get_most_recent(), [file_updates[0], file_updates[2]])
+        self.assertCountEqual(updates.get_most_recent(), [updates[0], updates[2]])
 
 
 if __name__ == "__main__":
---
+++ docker/test/dind/entrypoint.sh
@@ -0,0 +1,13 @@
+# Start Docker server within this Docker (hack: & used but dangerous as may not have started when required!)
+/usr/local/bin/dockerd-entrypoint.sh &
+
+# Setup Docker server (hack: should be possible to use this Docker as the server)
+docker run --privileged -d --name docker-server docker:1.9-dind
+docker exec docker-server apk add --update git
+
+# Hacky way of getting the address of the docker server
+SERVER_ADDRESS=$(docker run --link docker-server docker:1.9-dind env | grep "DOCKER_SERVER_PORT=" | cut -f2 -d "=")
+
+# Build then run the test runner
+docker build -t wtsi-hgi/cookie-monster/tests -f docker/tests/runner/Dockerfile .
+docker run --link docker-server -e DOCKER_HOST="$SERVER_ADDRESS" wtsi-hgi/cookie-monster/tests
