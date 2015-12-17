ef05583279417cdb359d6208f740e20a85a5a657
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-12-17 17:11:36 +0000

    Hack to stop Travis CI killing the DinD test running when it takes more than 10m to build an image.

--- docker/test/dind/entrypoint.sh
+++ docker/test/dind/entrypoint.sh
@@ -16,9 +16,12 @@ do
         exit 1
     fi
 done
-echo "\nDocker started"
+echo " started"
 
-# Hack: should be possible to use this Docker as the server
+echo "Building the test runner"
+docker build -t wtsi-hgi/cookie-monster/tests -f docker/test/runner/Dockerfile .
+
+ Hack: should be possible to use this Docker as the Docker server...
 echo "Starting Docker server"
 docker run --privileged -d --name docker-server docker:1.9-dind
 docker exec docker-server apk add --update git
@@ -26,9 +29,9 @@ echo "Docker server started"
 
 # Hacky way of getting the address of the docker server
 SERVER_ADDRESS=$(docker run --link docker-server docker:1.9-dind env | grep "DOCKER_SERVER_PORT=" | cut -f2 -d "=")
+#SERVER_ADDRESS="tcp://$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'):2375"
 
-echo "Building the test runner"
-docker build -t wtsi-hgi/cookie-monster/tests -f docker/test/runner/Dockerfile .
 echo "Starting the test runner"
+(while :; do echo "Docker container is still running..."; sleep 300; done) &
 docker run --link docker-server -e DOCKER_HOST="$SERVER_ADDRESS" wtsi-hgi/cookie-monster/tests
 echo "Complete"
