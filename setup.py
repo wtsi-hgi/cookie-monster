f603cf494b0f862eed71c73be033e7fc9c1dc9a0
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-01-08 16:30:59 +0000

    Added Python setup file.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cookie-monster",
+
+    version="0.1.0",
+
+    author="Sanger",
+    author_email="hgi@sanger.ac.uk",
+
+    packages=find_packages(exclude=["test"]),
+
+    url="https://github.com/wtsi-hgi/cookie-monster",
+
+    license="LICENSE",
+
+    description="Cookie Monster.",
+    long_description=open("README.md").read(),
+
+    install_requires=[x for x in open("requirements.txt").read().splitlines() if "://" not in x],
+    dependency_links=[x for x in open("requirements.txt").read().splitlines() if "://" in x]
+)
