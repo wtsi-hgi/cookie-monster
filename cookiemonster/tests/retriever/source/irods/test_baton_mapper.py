73e5071268ad9c7535f7d43afaf75d706997dd8f
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-01-15 15:05:03 +0000

    Added regression test for incorrect replica handling bug (https://github.com/wtsi-hgi/cookie-monster/issues/14).

--- cookiemonster/tests/retriever/source/irods/test_baton_mapper.py
+++ cookiemonster/tests/retriever/source/irods/test_baton_mapper.py
@@ -32,7 +32,7 @@ class TestBatonUpdateMapper(unittest.TestCase):
         self.test_with_baton.setup()
         self.setup_helper = SetupHelper(self.test_with_baton.icommands_location)
 
-        self._install_queries()
+        self._install_update_queries()
 
         self.mapper = BatonUpdateMapper(
                 self.test_with_baton.baton_location, self.test_with_baton.irods_test_server.users[0])
@@ -57,7 +57,17 @@ class TestBatonUpdateMapper(unittest.TestCase):
         self.assertEquals(len(updates.get_entity_updates(location_1)), 1)
         self.assertEquals(len(updates.get_entity_updates(location_2)), 1)
 
-    # XXX: No `test_get_all_since_with_collection_updates` as SQL query does not support collections
+    def test_get_all_since_with_updates_to_data_object_replica(self):
+        inital_updates = self.mapper.get_all_since(datetime.min)
+        location = self.setup_helper.create_data_object(_DATA_OBJECT_NAMES[0])
+
+        resource_name = self.setup_helper.create_replica_storage()
+        self.setup_helper.replicate_data_object(location, resource_name)
+
+        updates = self.mapper.get_all_since(inital_updates.get_most_recent()[0].timestamp)
+
+        self.assertEquals(len(updates), 1)
+        self.assertEquals(len(updates.get_entity_updates(location)), 1)
 
     def test_get_all_since_with_metadata_update(self):
         location = self.setup_helper.create_data_object(_DATA_OBJECT_NAMES[0])
@@ -81,9 +91,9 @@ class TestBatonUpdateMapper(unittest.TestCase):
     def tearDown(self):
         self.test_with_baton.tear_down()
 
-    def _install_queries(self):
+    def _install_update_queries(self):
         """
-        Installs the specific quries required to get file updates from iRODS.
+        Installs the specific queries required to get file updates from iRODS.
         """
         for alias, query_location_relative_to_root in REQUIRED_SPECIFIC_QUERIES.items():
             query_location = normpath(join(dirname(realpath(__file__)), "..", "..", "..", "..", "..",
