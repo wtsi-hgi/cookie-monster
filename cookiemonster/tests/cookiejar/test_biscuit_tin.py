dac2ad5db3b8fe62fc40e7cb9f2d5fbeeafbbe9c
Christopher Harrison <ch12@sanger.ac.uk>
2016-03-07 10:54:10 +0000

    Refactored tests for batching Cookie Jar
    Containerisation latency problem no longer an issue
    Fixes #22 (sort of)

--- cookiemonster/tests/cookiejar/test_biscuit_tin.py
+++ cookiemonster/tests/cookiejar/test_biscuit_tin.py
@@ -45,19 +45,18 @@ License
 GPLv3 or later
 Copyright (c) 2015, 2016 Genome Research Limited
 '''
+from datetime import datetime, timedelta
 import unittest
 from unittest.mock import MagicMock
 
 from cookiemonster.cookiejar import BiscuitTin
 from cookiemonster.tests._utils.docker_couchdb import CouchDBContainer
-
-from datetime import datetime, timedelta
-
-from hgicommon.collections import Metadata
 from cookiemonster.common.models import Enrichment, Cookie
 
+from hgicommon.collections import Metadata
+
+# We need these for mocking
 import cookiemonster.cookiejar._dbi as dbi
-from cookiemonster.cookiejar import BiscuitTin
 import cookiemonster.cookiejar.biscuit_tin as biscuit_tin
 
 
@@ -67,18 +66,20 @@ def _change_time(time):
 
 biscuit_tin.Timer = MagicMock()
 
+
 class TestCookieJar(unittest.TestCase):
     def setUp(self):
         '''
         Build, if necessary, and start a Dockerised CouchDB instance and
         connect. Plus, provide sample inputs with which to test.
         '''
-        #self.couchdb_container = CouchDBContainer()
+        self.couchdb_container = CouchDBContainer()
 
-        #self.HOST = self.couchdb_container.couchdb_fqdn
-        self.HOST = 'http://localhost:5984'
+        self.HOST = self.couchdb_container.couchdb_fqdn
         self.DB   = 'cookiejar-test'
 
+        # TODO? We don't test the buffering (only the trivial case of a
+        # single document, zero-latency buffer)
         self.jar = BiscuitTin(self.HOST, self.DB, 1, timedelta(0))
 
         self.eg_paths = [
@@ -91,7 +92,7 @@ class TestCookieJar(unittest.TestCase):
         ]
         self.eg_enrichments = [
             Enrichment('random', datetime(1981, 9, 25, 5, 55), self.eg_metadata[0]),
-            Enrichment("irods", datetime(2015, 12, 9, 9), self.eg_metadata[1])
+            Enrichment("irods", datetime.now().replace(microsecond=0), self.eg_metadata[1])
         ]
         self.eg_listener = MagicMock()
 
@@ -102,8 +103,8 @@ class TestCookieJar(unittest.TestCase):
 
     def tearDown(self):
         ''' Tear down CouchDB container '''
-        #self.couchdb_container.tear_down()
-        self.jar._sofa._db._server.delete(self.DB)
+        self.couchdb_container.tear_down()
+        biscuit_tin.Timer.reset_mock()
 
     def test01_empty_queue(self):
         '''
@@ -119,7 +120,7 @@ class TestCookieJar(unittest.TestCase):
         '''
         self.jar.enrich_cookie(self.eg_paths[0], self.eg_enrichments[0])
         self.assertEqual(self.jar.queue_length(), 1)
-        
+
         to_process = self.jar.get_next_for_processing()
 
         self.assertEqual(self.jar.queue_length(), 0)
@@ -217,12 +218,16 @@ class TestCookieJar(unittest.TestCase):
         '''
         self.jar.enrich_cookie(self.eg_paths[0], self.eg_enrichments[0])
         before = self.jar.get_next_for_processing()
-        self.jar.mark_as_failed(before.path, timedelta(0))
+        self.jar.mark_as_failed(before.path)
+
+        # Test that the queue length broadcast has been scheduled and
+        # the queue length has changed appropriately
+        biscuit_tin.Timer.assert_called_with(0, self.jar._broadcast_length)
         self.assertEqual(self.jar.queue_length(), 1)
+
         after = self.jar.get_next_for_processing()
         self.assertEqual(self.jar.queue_length(), 0)
         self.assertEqual(before, after)
-        self.assertEqual(biscuit_tin.Timer.call_args[0][0], 0)
 
     def test08_fail_delayed(self):
         '''
@@ -234,6 +239,9 @@ class TestCookieJar(unittest.TestCase):
         self.jar.mark_as_failed(to_process.path, timedelta(seconds=3))
         self.assertEqual(self.jar.queue_length(), 0)
 
+        # Test that the queue length broadcast has been scheduled
+        biscuit_tin.Timer.assert_called_with(3, self.jar._broadcast_length)
+
         # +1 second
         _change_time(123457)
         self.assertEqual(self.jar.queue_length(), 0)
@@ -242,13 +250,16 @@ class TestCookieJar(unittest.TestCase):
         _change_time(123458)
         self.assertEqual(self.jar.queue_length(), 0)
 
-        # +3 seconds
+        # +3 seconds: Queue length should change
         _change_time(123459)
         self.assertEqual(self.jar.queue_length(), 1)
 
-        # We only test that the queue length broadcast has been
-        # scheduled; we assume that the method will get called
-        self.assertEqual(biscuit_tin.Timer.call_args[0][0], 3)
+        # FIXME? This *doesn't* test that the listener is called at the
+        # appropriate time with the appropriate arguments; it just shows
+        # that the listener will be called after the appropriate time
+        # and that, at that time, the queue length (i.e., its argument)
+        # is correct. There could be possible synchronisation issues due
+        # to the inexactness of the Timer.
 
     def test09_out_of_order_enrichment(self):
         '''
@@ -296,7 +307,7 @@ class TestCookieJar(unittest.TestCase):
         new_jar = BiscuitTin(self.HOST, self.DB, 1, timedelta(0))
 
         self.assertEqual(new_jar.queue_length(), 1)
-        
+
         to_process = new_jar.get_next_for_processing()
 
         self.assertEqual(new_jar.queue_length(), 0)
@@ -314,7 +325,7 @@ class TestCookieJar(unittest.TestCase):
 
         new_jar = BiscuitTin(self.HOST, self.DB, 1, timedelta(0))
         self.assertEqual(new_jar.queue_length(), 1)
-        
+
         after = new_jar.get_next_for_processing()
         self.assertEqual(before, after)
 
