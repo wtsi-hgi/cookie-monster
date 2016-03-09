290388e0da16952a4360f1a62b123754e907bd76
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-03-09 14:15:11 +0000

    Refactored logger.

--- cookiemonster/logging/influxdb/logger.py
+++ cookiemonster/logging/influxdb/logger.py
@@ -1,9 +1,7 @@
-import atexit
 import logging
 from datetime import timedelta
 from json import JSONEncoder
-from threading import Lock, Timer
-from typing import Dict, Optional, List
+from typing import Dict, Iterable, List
 
 from influxdb import InfluxDBClient
 
@@ -20,7 +18,7 @@ class InfluxDBLogger(Logger):
     _INFLUXDB_LOG_JSON_ENCODER = InfluxDBLogJSONEncoder()   # type: JSONEncoder
 
     def __init__(self, connection_config: InfluxDBConnectionConfig, static_tags: Dict=None,
-                 buffer_latency: Optional[timedelta]=timedelta(seconds=10)):
+                 buffer_latency: timedelta=None):
         """
         Constructor.
         :param connection_config: connection configuration for the InfluxDB database
@@ -28,52 +26,25 @@ class InfluxDBLogger(Logger):
         :param buffer_latency: the time in which the log buffer is filled before logs are written. Set to `None` if
         logs should be written straight away.
         """
+        super().__init__(buffer_latency)
         self.static_tags = static_tags if static_tags is not None else dict()
-        self.buffer_latency = buffer_latency
 
         self._influxdb_client = InfluxDBClient(connection_config.host, connection_config.port, connection_config.user,
                                                connection_config.password, connection_config.database)
-        self._buffer = []    # type: List[Log]
-        self._buffer_timer = None   # type: Optional[Timer]
-        self._buffer_lock = Lock()
 
-        # Flush the buffer on exit
-        atexit.register(self.flush)
+    def _write_logs(self, logs: Iterable[Log]):
+        influxdb_logs_as_json = []     # type: List[Dict]
+        for log in logs:
+            log.metadata.update(self.static_tags)
+            influxdb_log_as_json = InfluxDBLogger._convert_log_to_json(log)
+            influxdb_logs_as_json.append(influxdb_log_as_json)
 
-    def flush(self):
-        """
-        Flush any logs from the buffer.
-        """
-        with self._buffer_lock:
-            influxdb_logs_as_json_list = []     # type: List[Dict]
-            for log in self._buffer:
-                influxdb_logs_as_json_list.append(InfluxDBLogger._convert_log_to_json(log))
-            self._buffer.clear()
-
-            if self._buffer_timer is not None and self._buffer_timer.is_alive():
-                self._buffer_timer.cancel()
-            self._buffer_timer = None
-
-        logging.debug("Logging: %s" % influxdb_logs_as_json_list)
-        if len(influxdb_logs_as_json_list) > 0:
-            successful = self._influxdb_client.write_points(influxdb_logs_as_json_list)
-            if not successful:
-                logging.error("Error when sending logs to InfluxDB (%d logs were lost)"
-                              % len(influxdb_logs_as_json_list))
-            else:
-                logging.info("%d logs successfully sent to InfluxDB" % len(influxdb_logs_as_json_list))
-
-    def _process_log(self, log: Log):
-        log.metadata.update(self.static_tags)
-
-        with self._buffer_lock:
-            self._buffer.append(log)
-            if self._buffer_timer is None and self.buffer_latency is not None:
-                self._buffer_timer = Timer(self.buffer_latency.total_seconds(), self.flush)
-                self._buffer_timer.start()
-
-        if self.buffer_latency is None or self.buffer_latency.total_seconds() == 0:
-            self.flush()
+        logging.debug("Writing logs: %s" % influxdb_logs_as_json)
+        successful = self._influxdb_client.write_points(influxdb_logs_as_json)
+        if not successful:
+            logging.error("Error when sending logs to InfluxDB (%d logs were lost)" % len(influxdb_logs_as_json))
+        else:
+            logging.info("%d log(s) sent successfully to InfluxDB" % len(influxdb_logs_as_json))
 
     @staticmethod
     def _convert_log_to_json(log: Log) -> Dict:
