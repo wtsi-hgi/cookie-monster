7668f422a71840a0a34333aced9f8655a6ca101d
Christopher Harrison <ch12@sanger.ac.uk>
2016-03-14 15:06:27 +0000

    Fleshed out tests a bit

--- cookiemonster/tests/common/decorators/test_gotta_catchem_all.py
+++ cookiemonster/tests/common/decorators/test_gotta_catchem_all.py
@@ -9,7 +9,11 @@ from cookiemonster.common.decorators import too_big_to_fail, MaxAttemptsExhauste
 class ABCStub(metaclass=ABCMeta):
     @abstractmethod
     def this_will_fail_unless(self, condition:bool) -> bool:
-        ''' This method will raise an exception until condition is True '''
+        ''' This method will raise an exception unless condition is True '''
+
+    @abstractmethod
+    def this_will_always_fail(self):
+        ''' This method will always raise an exception '''
 
 @too_big_to_fail
 class Stub(ABCStub):
@@ -19,6 +23,9 @@ class Stub(ABCStub):
 
         return True
 
+    def this_will_always_fail(self):
+        raise Exception
+
 
 class TestCatcherDecorator(unittest.TestCase):
     def test_fail_after_max_attempts(self):
@@ -27,10 +34,15 @@ class TestCatcherDecorator(unittest.TestCase):
 
         for x in range(1, tries + 1):
             if x == tries:
-                self.assertEqual(obj.this_will_fail_unless(x == tries), True)
+                self.assertTrue(obj.this_will_fail_unless(x == tries))
             else:
                 with self.assertRaises(MaxAttemptsExhausted):
                     obj.this_will_fail_unless(x == tries)
+
+    @unittest.skip('How do you test something that can never fail!?')
+    def test_never_fails(self):
+        obj = Stub()
+
 
 if __name__ == '__main__':
     unittest.main()
