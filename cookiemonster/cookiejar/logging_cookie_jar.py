f59f1f7cee26480cebed588a18ec6d4084936a15
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-03-14 12:32:03 +0000

    Corrected logging cookie jar implementation.

--- cookiemonster/cookiejar/logging_cookie_jar.py
+++ cookiemonster/cookiejar/logging_cookie_jar.py
@@ -1,99 +1,48 @@
-from typing import Optional, Callable, Any
-
-from datetime import timedelta
 import time
+from functools import wraps
+from typing import Callable
 
-from hgicommon.models import Model
-
-from cookiemonster import Cookie, Enrichment
 from cookiemonster.cookiejar import CookieJar
 from cookiemonster.logging.logger import Logger
 
+MEASUREMENT_QUERY_TIME = {
+    CookieJar.enrich_cookie.__name__: "enrich_cookie_time",
+    CookieJar.mark_as_failed.__name__: "mark_as_failed_time",
+    CookieJar.mark_as_complete.__name__: "mark_as_complete_time",
+    CookieJar.mark_for_processing.__name__: "mark_for_processing",
+    CookieJar.get_next_for_processing.__name__: "get_next_for_processing_time",
+    CookieJar.queue_length.__name__: "queue_length_time"
+}
 
-MEASUREMENT_ENRICH_COOKIE_TIME = "enrich_cookie_time"
-MEASUREMENT_MARK_AS_FAILED_TIME = "mark_as_failed_time"
-MEASUREMENT_MARK_AS_COMPLETE_TIME = "mark_as_complete_time"
-MEASUREMENT_MARK_FOR_PROCESSING_TIME = "mark_for_processing"
-MEASUREMENT_GET_NEXT_FOR_PROCESSING_TIME = "get_next_for_processing_time"
-MEASUREMENT_QUEUE_LENGTH_TIME = "queue_length_time"
 
-_DurationInSeconds = int
-
-
-class _TimedFunctionCallResult(Model):
+def _timer_wrap(method_name: str, method: Callable, logger: Logger) -> Callable:
     """
-    Model of a timed function call result.
+    Wraps the given method such that the time taken to complete the call to it is timed and then logged.
+    :param method_name: the name of the method that is to be wrapped
+    :param method: the method that is to be wrapped
+    :param logger: where to log timings to
     """
-    def __init__(self, duration: _DurationInSeconds, return_value: Any):
-        self.duration = duration
-        self.return_value = return_value
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        started_at = time.monotonic()
+        return_value = method(*args, **kwargs)
+        duration = time.monotonic() - started_at
+        logger.record(MEASUREMENT_QUERY_TIME[method_name], duration)
+        return return_value
+    return wrapper
 
 
-def _time_method_call(fn: Callable, *function_args, **function_kwargs) -> _TimedFunctionCallResult:
-    """
-    Times the given function call.
-    :param fn: the function to time
-    :param function_args: the positional arguments to give to the function that is been timed
-    :param function_kwargs: the named arguments to give to the function that is been timed
-    :return: the return of the function along with the time taken to complete the call
-    """
-    # TODO: Look if there is a library that does this when have Internet...
-    started_at = time.monotonic()
-    return_value = fn(*function_args, **function_kwargs)
-    duration = time.monotonic() - started_at
-    return _TimedFunctionCallResult(duration, return_value)
-
-
-class LoggingCookieJar(CookieJar):
+def logging_cookie_jar(cookie_jar: CookieJar, logger: Logger) -> CookieJar:
     """
     CookieJar decorator that wraps a given `CookieJar` implementation and logs the time taken to complete calls to its
     functions.
+
+    Modifies the given `CookieJar` instance.
+    :param cookie_jar: the `CookieJar` to decorate
+    :param logger: where to log query times to
+    :return: decorated `CookieJar`
     """
-    def __init__(self, cookie_jar: CookieJar, logger: Logger):
-        """
-        Constructor.
-        :param cookie_jar: the `CookieJar` implementation to log method calls for
-        :param logger: the log recorder
-        """
-        super().__init__()
-        self._super_properties = dir(super())
-        self._composite_cookie_jar = cookie_jar
-        self._logger = logger
+    for method_name in CookieJar.__abstractmethods__:
+        setattr(cookie_jar, method_name, _timer_wrap(method_name, getattr(cookie_jar, method_name), logger))
 
-    def __getattribute__(self, attribute: str):
-        # This override ensures methods/properties of composite are used unless defined in the adapter. Does not apply
-        # to magic methods though
-        local_property = attribute in object.__getattribute__(self, "__dict__").keys()
-        super_property = attribute in object.__getattribute__(self, "_super_properties")
-        magic_property = attribute.startswith("__")
-
-        if attribute in vars(LoggingCookieJar) or (magic_property or local_property and not super_property):
-            return object.__getattribute__(self, attribute)
-        else:
-            return type(self._composite_cookie_jar).__getattribute__(self._composite_cookie_jar, attribute)
-
-    def enrich_cookie(self, identifier: str, enrichment: Enrichment):
-        result = _time_method_call(self._composite_cookie_jar.enrich_cookie, identifier, enrichment)
-        self._logger.record(MEASUREMENT_ENRICH_COOKIE_TIME, result.duration)
-
-    def mark_as_failed(self, identifier: str, requeue_delay: timedelta):
-        result = _time_method_call(self._composite_cookie_jar.mark_as_failed, identifier, requeue_delay)
-        self._logger.record(MEASUREMENT_MARK_AS_FAILED_TIME, result.duration)
-
-    def mark_as_complete(self, identifier: str):
-        result = _time_method_call(self._composite_cookie_jar.mark_as_complete, identifier)
-        self._logger.record(MEASUREMENT_MARK_AS_COMPLETE_TIME, result.duration)
-
-    def mark_for_processing(self, identifier: str):
-        result = _time_method_call(self._composite_cookie_jar.mark_for_processing, identifier)
-        self._logger.record(MEASUREMENT_MARK_FOR_PROCESSING_TIME, result.duration)
-
-    def get_next_for_processing(self) -> Optional[Cookie]:
-        result = _time_method_call(self._composite_cookie_jar.get_next_for_processing)
-        self._logger.record(MEASUREMENT_GET_NEXT_FOR_PROCESSING_TIME, result.duration)
-        return result.return_value
-
-    def queue_length(self) -> int:
-        result = _time_method_call(self._composite_cookie_jar.queue_length)
-        self._logger.record(MEASUREMENT_QUEUE_LENGTH_TIME, result.duration)
-        return result.return_value
+    return cookie_jar
--- cookiemonster/tests/cookiejar/test_logging_cookie_jar.py
+++ cookiemonster/tests/cookiejar/test_logging_cookie_jar.py
@@ -1,14 +1,14 @@
 import unittest
 from datetime import datetime, timedelta
+from typing import Dict
 from unittest.mock import MagicMock
 
 from hgicommon.collections import Metadata
 
 from cookiemonster import Enrichment
+from cookiemonster.cookiejar import CookieJar
 from cookiemonster.cookiejar.in_memory_cookiejar import InMemoryCookieJar
-from cookiemonster.cookiejar.logging_cookie_jar import LoggingCookieJar, MEASUREMENT_GET_NEXT_FOR_PROCESSING_TIME, \
-    MEASUREMENT_ENRICH_COOKIE_TIME, MEASUREMENT_MARK_AS_FAILED_TIME, MEASUREMENT_MARK_AS_COMPLETE_TIME, \
-    MEASUREMENT_QUEUE_LENGTH_TIME, MEASUREMENT_MARK_FOR_PROCESSING_TIME
+from cookiemonster.cookiejar.logging_cookie_jar import logging_cookie_jar, MEASUREMENT_QUERY_TIME
 
 
 class TestLoggingCookieJar(unittest.TestCase):
@@ -16,63 +16,70 @@ class TestLoggingCookieJar(unittest.TestCase):
     Tests for `LoggingCookieJar`.
     """
     def setUp(self):
-        self._composite_cookie_jar = MagicMock()
         self._logger = MagicMock()
-        self._cookie_jar = LoggingCookieJar(self._composite_cookie_jar, self._logger)
+
+        self._composite_cookie_jar = MagicMock()
+        self._composite_methods = dict()    # type: Dict[str, MagicMock]
+        for method_name in CookieJar.__abstractmethods__:
+            method = getattr(self._composite_cookie_jar, method_name)
+            self._composite_methods[method_name] = method
+
+        self._cookie_jar = logging_cookie_jar(self._composite_cookie_jar, self._logger)
 
     def test_enrich_cookie(self):
         source = "my_source"
         enrichment = Enrichment("source", datetime.min, Metadata())
         self._cookie_jar.enrich_cookie(source, enrichment)
-        self._composite_cookie_jar.enrich_cookie.assert_called_once_with(source, enrichment)
-        self._assert_measured(MEASUREMENT_ENRICH_COOKIE_TIME)
+        self._composite_methods[CookieJar.enrich_cookie.__name__].assert_called_once_with(source, enrichment)
+        self._assert_measured(MEASUREMENT_QUERY_TIME[CookieJar.enrich_cookie.__name__])
 
     def test_mark_as_failed(self):
         identifier = "identifier"
         requeue_delay = timedelta(seconds=5)
         self._cookie_jar.mark_as_failed(identifier, requeue_delay)
-        self._composite_cookie_jar.mark_as_failed.assert_called_once_with(identifier, requeue_delay)
-        self._assert_measured(MEASUREMENT_MARK_AS_FAILED_TIME)
+        self._composite_methods[CookieJar.mark_as_failed.__name__].assert_called_once_with(identifier, requeue_delay)
+        self._assert_measured(MEASUREMENT_QUERY_TIME[CookieJar.mark_as_failed.__name__])
 
     def test_mark_as_complete(self):
         identifier = "identifier"
         self._cookie_jar.mark_as_complete(identifier)
-        self._composite_cookie_jar.mark_as_complete.assert_called_once_with(identifier)
-        self._assert_measured(MEASUREMENT_MARK_AS_COMPLETE_TIME)
+        self._composite_methods[CookieJar.mark_as_complete.__name__].assert_called_once_with(identifier)
+        self._assert_measured(MEASUREMENT_QUERY_TIME[CookieJar.mark_as_complete.__name__])
 
     def test_mark_for_processing(self):
         identifier = "identifier"
         self._cookie_jar.mark_for_processing(identifier)
-        self._composite_cookie_jar.mark_for_processing.assert_called_once_with(identifier)
-        self._assert_measured(MEASUREMENT_MARK_FOR_PROCESSING_TIME)
+        self._composite_methods[CookieJar.mark_for_processing.__name__].assert_called_once_with(identifier)
+        self._assert_measured(MEASUREMENT_QUERY_TIME[CookieJar.mark_for_processing.__name__])
 
     def test_get_next_for_processing(self):
         self._cookie_jar.get_next_for_processing()
-        self._composite_cookie_jar.get_next_for_processing.assert_called_once_with()
-        self._assert_measured(MEASUREMENT_GET_NEXT_FOR_PROCESSING_TIME)
+        self._composite_methods[CookieJar.get_next_for_processing.__name__].assert_called_once_with()
+        self._assert_measured(MEASUREMENT_QUERY_TIME[CookieJar.get_next_for_processing.__name__])
 
     def test_queue_length(self):
         self._cookie_jar.queue_length()
-        self._composite_cookie_jar.queue_length.assert_called_once_with()
-        self._assert_measured(MEASUREMENT_QUEUE_LENGTH_TIME)
+        self._composite_methods[CookieJar.queue_length.__name__].assert_called_once_with()
+        self._assert_measured(MEASUREMENT_QUERY_TIME[CookieJar.queue_length.__name__])
 
     def test_call_to_non_adapted_cookie_jar_method(self):
-        self._composite_cookie_jar = InMemoryCookieJar()
-        self._cookie_jar._composite_cookie_jar = self._composite_cookie_jar
+        self._cookie_jar = InMemoryCookieJar()
+        self._cookie_jar._composite_cookie_jar = self._cookie_jar
 
         listener = lambda: None
-        self._composite_cookie_jar.add_listener(listener)
-        assert self._composite_cookie_jar.get_listeners() == [listener]
-        self.assertEqual(self._cookie_jar.get_listeners, self._composite_cookie_jar.get_listeners)
+        self._cookie_jar.add_listener(listener)
+        assert self._cookie_jar.get_listeners() == [listener]
+        self.assertEqual(self._cookie_jar.get_listeners, self._cookie_jar.get_listeners)
         self.assertEqual(self._cookie_jar.get_listeners(), [listener])
 
     def test_call_to_non_cookie_jar_method(self):
-        self._composite_cookie_jar.some_method()
-        self._composite_cookie_jar.some_method.assert_called_once_with()
+        self._cookie_jar.some_method = MagicMock()
+        self._cookie_jar.some_method()
+        self._cookie_jar.some_method.assert_called_once_with()
 
     def test_call_to_non_cookie_jar_property(self):
-        self._composite_cookie_jar.some_property = 1
-        self.assertEqual(self._composite_cookie_jar.some_property, 1)
+        self._cookie_jar.some_property = 1
+        self.assertEqual(self._cookie_jar.some_property, 1)
 
     def _assert_measured(self, measured: str):
         calls = self._logger.record.mock_calls
