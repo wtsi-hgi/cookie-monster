224e2f91760754264bfe0829f477a37ed602866a
Christopher Harrison <ch12@sanger.ac.uk>
2016-05-26 12:20:05 +0100

    Lock that counts threads trying to acquire itself

--- cookiemonster/cookiejar/biscuit_tin.py
+++ cookiemonster/cookiejar/biscuit_tin.py
@@ -118,6 +118,42 @@ def _now() -> int:
     return int(time())
 
 
+class _CountingLock(object):
+    """ Lock that keeps count of threads waiting to acquire itself """
+    def __init__(self, *args, **kwargs):
+        """ Wraps Lock constructor """
+        self._lock = Lock(*args, **kwargs)
+
+        self._wait_lock = Lock()
+        self._waiting = 0
+
+    def acquire(self, *args, **kwargs):
+        """ Wraps Lock.acquire """
+        with self._wait_lock:
+            self._waiting += 1
+
+        self._lock.acquire(*args, **kwargs)
+
+        with self._wait_lock:
+            self._waiting -= 1
+
+    def release(self):
+        """ Wraps Lock.release """
+        self._lock.release()
+
+    def waiting_to_acquire(self) -> int:
+        """ Return the number of threads waiting to acquire the lock """
+        with self._wait_lock:
+            waiting = self._waiting
+        return waiting
+
+    def __enter__(self):
+        self.acquire()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.release()
+
+
 class _Bert(object):
     """ Interface to the queue database documents """
     @staticmethod
