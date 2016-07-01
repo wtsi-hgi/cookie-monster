7b4343936e5cac6e584d6d9c73a4b0bb805ac9e5
Christopher Harrison <ch12@sanger.ac.uk>
2016-07-01 14:30:43 +0100

    CouchDB response time logging using generic injector framework

--- cookiemonster/cookiejar/couchdb/dream_diary.py
+++ cookiemonster/cookiejar/couchdb/dream_diary.py
@@ -1,21 +1,16 @@
 """
 Logging Dependency Injection
 ============================
-Provides runtime logging for CouchDB calls performed by SofterCouchDB
-(in cookiemonster.cookiejar.couchdb.softer) via dependency injection
+Provides response time logging for CouchDB calls performed by 
+SofterCouchDB (in cookiemonster.cookiejar.couchdb.softer) via dependency
+injection
 
 Exportable functions: `inject_logging`
-Exportable classes: `LoggingSofterCouchDB`
 
 inject_logging
 --------------
 `inject_logging` will apply runtime logging to an instantiated
 SofterCouchDB's methods, using the specified logger.
-
-LoggingSofterCouchDB
---------------------
-`LoggingSofterCouchDB` instantiates a `SofterCouchDB` with logging
-applied to its methods.
 
 Legalese
 --------
@@ -38,45 +33,37 @@ Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-import time
-from typing import Any, Callable
+from typing import Dict, Optional
 
 from cookiemonster.cookiejar.couchdb.softer import SofterCouchDB
 from cookiemonster.logging.logger import Logger
+from cookiemonster.logging.injector import LoggingContext, RuntimeLogging, LoggingMapper
 
 
-def _log_runtime(fn:Callable[..., Any], logger:Logger) -> Callable[..., Any]:
+class _CouchDBResponseTimeLogging(RuntimeLogging):
     """
-    Decorates the function with runtime logging
-
-    @param   fn      The function to decorate
-    @param   logger  The logger to write to
-    @return  Decorated function
+    Log response times under couchdb with a "function" tag matching the
+    called CouchDB function (e.g., save_bulk, etc.)
     """
-    def wrapper(*args, **kwargs):
-        start = time.monotonic()
-        output = fn(*args, **kwargs)
-        runtime = time.monotonic() - start
+    def get_measure(self, context:LoggingContext) -> str:
+        return 'couchdb'
 
-        logger.record('couchdb_runtime', runtime, {'function': fn.__name__})
-        return output
-
-    return wrapper
+    def get_metadata(self, context:LoggingContext) -> Optional[Dict]:
+        return {'function': context.fn.__name__}
 
 
 def inject_logging(db:SofterCouchDB, logger:Logger):
     """
-    Inject runtime logging into a SofterCouchDB instance, targeting all
-    callable, "public" attributes
+    Inject CouchDB response time logging into a SofterCouchDB instance,
+    targeting all callable, "public" attributes. Note that we can only
+    inject into instantiated SofterCouchDBs because they are monkey-
+    patched themselves; as such, we can't use the mass mapping
+    convenience functions of the injector.
 
     @param   db      The SofterCouchDB instance to inject into
     @param   logger  The logger to write to
     """
+    mapping = LoggingMapper(logger)
     for method in db._db_methods:
-        setattr(db, method, _log_runtime(getattr(db, method), logger))
-
-
-class LoggingSofterCouchDB(SofterCouchDB):
-    def __init__(self, logger, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        inject_logging(self, logger)
+        mapping.map_logging_to_method(method, _CouchDBResponseTimeLogging)
+    mapping.inject_logging(db)
